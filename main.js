1c97df6456ea2b7c98dd7f93919e5d6302935585
FatB0YY <60257066+FatB0YY@users.noreply.github.com>
2021-02-07 16:30:08 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -56,9 +56,9 @@ document.addEventListener("DOMContentLoaded", function() {
       <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
       <polyline points="1 9 7 14 15 4"></polyline>
   </svg>${item.todo}</label>
-            <button class="history__important" data-id="${
-              item.idImp
-            }">&#9733;</button>
+            <button class="history__important ${
+              item.important ? "important" : ""
+            }" data-id="${item.idImp}">&#9733;</button>
             <button class="history__delete" data-id="${item.idDel}">x</button>
         </li>
             `;
